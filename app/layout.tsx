a10b587efb71d72b448a4ee5f512cd1fa79bd19e
m4cone <boffmarcone@gmail.com>
2025-02-11 18:01:44 -0300

    add `metadata` title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import { inter } from "./ui/fonts";
 import "./ui/global.css";
+import { Metadata } from "next";
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
+export const metadata: Metadata = {
+  title: "Notes Tab",
+  description: "Create notes in a simplified way.",
+};
 
 export default function RootLayout({
   children,
